534f029978e7f18f8ff0d14a5b248d64f84a23c5
Arey125 <andrej.kurenckoff@gmail.com>
2025-06-16 16:55:29 +0300

    feat: static files are now embedded

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,7 @@ import (
 	database "metro/internal/db"
 	"metro/internal/metro"
 	"metro/internal/users"
+    "metro/static"
 
 	"github.com/alexedwards/scs/sqlite3store"
 	"github.com/alexedwards/scs/v2"
@@ -24,7 +25,7 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	staticFileServer := http.FileServer(http.Dir("./static"))
+	staticFileServer := http.FileServer(http.FS(static.StaticFiles))
 	mux.Handle("GET /static/", http.StripPrefix("/static", staticFileServer))
 
 	usersModel := users.NewModel(db)
